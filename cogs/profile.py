38dbd1c10df82e37b941a05aa15c2d11123f2b79
Marvin Grunwald <marvin.grunwald@students.tbs1.de>
2022-01-31 20:29:12 +0100

    lvl bar

--- cogs/profile.py
+++ cogs/profile.py
@@ -50,7 +50,7 @@ class Profile(commands.Cog):
             if fetched_xp >= calculate_xp:
                 db.database.execute(f'UPDATE userdata SET lvl = lvl + 1 WHERE d_id = {message.author.id}')
                 db.database.execute(f'UPDATE userdata SET xp = 0 WHERE d_id = {message.author.id}')
-                db.database.execute(f'UPDATE userdata SET growth = growth + 0.05 WHERE d_id = {message.author.id}')
+                db.database.execute(f'UPDATE userdata SET growth = growth + 0.025 WHERE d_id = {message.author.id}')
                 db.database.execute(f'UPDATE userdata SET money = money + 50 WHERE d_id = {message.author.id}')
                 embedVar = discord.Embed(title="Level Up!", description=f'{message.author.name} hat ein neues Level erreicht!', color=0x0000CD)
                 embedVar.set_thumbnail(url="https://i.redd.it/5ej93xbz1jo51.gif")
@@ -88,6 +88,23 @@ class Profile(commands.Cog):
                 embedVar.set_footer(text=f"📅 Beitritt am: {result[0][6]}")
 
         else:
+            db.database.execute(f'SELECT * FROM userdata WHERE d_id = {ctx.author.id}')
+            result = db.database.fetchall()
+            try:
+              fetched_xp, fetched_growth, fetched_level = result[0][8], result[0][9], result[0][3]
+            except IndexError:
+              print(result)
+            calculate_xp = 50 * (1+fetched_growth) ** int(fetched_level)
+            calculated_percentage = int(((fetched_xp - 0) * 100) / (int(calculate_xp) - 0))
+            calculated_blues = calculated_percentage // 10
+            xp_bar = ""
+
+            for blues in range(calculated_blues):
+                xp_bar += "🟦"
+            
+            for whites in range(10-calculated_blues):
+                xp_bar += "⬜"
+
             user = ctx.author
             user_id = ctx.author.id
             db.database.execute(f'SELECT * FROM userdata WHERE d_id = {user_id}')
@@ -96,7 +113,84 @@ class Profile(commands.Cog):
             embedVar.set_thumbnail(url=ctx.author.avatar_url)
             embedVar.add_field(name="💬 Messages", value=result[0][5], inline=True)
             embedVar.add_field(name="⚡ Level", value=result[0][3], inline=True)
-            embedVar.add_field(name="\u200b", value="\u200b", inline=True)
+            embedVar.add_field(name="Progress", value=xp_bar)
+            embedVar.add_field(name="💴 Money", value=result[0][7], inline=True)
+            embedVar.add_field(name="ID", value=result[0][2], inline=True)
+            embedVar.add_field(name="So oft wurde dir Geld geklaut", value=result[0][11], inline=False)
+            embedVar.add_field(name="Entwichene Raubversuche", value=result[0][12], inline=True)
+            embedVar.add_field(name="Gearbeitete Stunden", value=result[0][13], inline=True)
+            #embedVar.add_field(name="\u200b", value="\u200b")
+
+            role_mention = []
+            for role in user.roles:
+                if role.name != "@everyone":
+                    role_mention.append(role.mention)
+            role_string = ", ".join(role_mention)
+            embedVar.add_field(name="Roles", value=role_string, inline=False)
+
+            embedVar.add_field(name="\u200b", value="\u200b")
+            embedVar.add_field(name="\u200b", value="\u200b")
+            embedVar.set_footer(text=f"📅 Beitritt am: {result[0][6]}")
+
+        await ctx.send(embed=embedVar)
+
+    @commands.command()
+    async def testprofile(self, ctx, *args:discord.Member):
+        if len(args) > 0:
+            for i in args:
+                db.database.execute(f'SELECT * FROM userdata WHERE d_id = {i.id}')
+                result = db.database.fetchall()
+
+                embedVar = discord.Embed(title=i.display_name, description=result[0][10], color=0x0000CD)
+                embedVar.set_thumbnail(url=i.avatar_url)
+                embedVar.add_field(name="💬 Messages", value=result[0][5], inline=True)
+                embedVar.add_field(name="⚡ Level", value=result[0][3], inline=True)
+                embedVar.add_field(name="\u200b", value="\u200b", inline=True)
+                embedVar.add_field(name="💴 Money", value=result[0][7], inline=True)
+                embedVar.add_field(name="ID", value=result[0][2], inline=True)
+                embedVar.add_field(name="So oft wurde dir Geld geklaut", value=result[0][11], inline=False)
+                embedVar.add_field(name="Entwichene Raubversuche", value=result[0][12], inline=True)
+                embedVar.add_field(name="Gearbeitete Stunden", value=result[0][13], inline=True)
+                #embedVar.add_field(name="\u200b", value="\u200b", inline=True)
+
+                role_mention = []
+                for role in i.roles:
+                    if role.name != "@everyone":
+                        role_mention.append(role.mention)
+                role_string = ", ".join(role_mention)
+                embedVar.add_field(name="Roles", value=role_string, inline=False)
+
+                embedVar.add_field(name="\u200b", value="\u200b")
+                embedVar.add_field(name="\u200b", value="\u200b")
+                embedVar.set_footer(text=f"📅 Beitritt am: {result[0][6]}")
+
+        else:
+            db.database.execute(f'SELECT * FROM userdata WHERE d_id = {ctx.author.id}')
+            result = db.database.fetchall()
+            try:
+              fetched_xp, fetched_growth, fetched_level = result[0][8], result[0][9], result[0][3]
+            except IndexError:
+              print(result)
+            calculate_xp = 50 * (1+fetched_growth) ** int(fetched_level)
+            calculated_percentage = int(((fetched_xp - 0) * 100) / (int(calculate_xp) - 0))
+            calculated_blues = calculated_percentage // 10
+            xp_bar = ""
+
+            for blues in range(calculated_blues):
+                xp_bar += "🟦"
+            
+            for whites in range(10-calculated_blues):
+                xp_bar += "⬜"
+
+            user = ctx.author
+            user_id = ctx.author.id
+            db.database.execute(f'SELECT * FROM userdata WHERE d_id = {user_id}')
+            result = db.database.fetchall()
+            embedVar = discord.Embed(title=ctx.author.display_name, description=result[0][10], color=0x0000CD)
+            embedVar.set_thumbnail(url=ctx.author.avatar_url)
+            embedVar.add_field(name="💬 Messages", value=result[0][5], inline=True)
+            embedVar.add_field(name="⚡ Level", value=result[0][3], inline=True)
+            embedVar.add_field(name="Progress", value=xp_bar)
             embedVar.add_field(name="💴 Money", value=result[0][7], inline=True)
             embedVar.add_field(name="ID", value=result[0][2], inline=True)
             embedVar.add_field(name="So oft wurde dir Geld geklaut", value=result[0][11], inline=False)
